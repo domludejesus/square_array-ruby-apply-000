69ed57d51076d32ffe38ff52772b5667949f7468
Dominic DeJesus <domludejesus@gmail.com>
2020-04-08 16:14:33 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,19 @@
-def square_array(array)
-  # your code here
+def square_array(nums)
+  new_array = []
+  nums.each{|x| new_array.push(x**2)}
+  return new_array
 end
+
+nums = [8, 6, 4, 2, 1]
+square_array(nums)
+
+ 
+ 
+
+ 
+ 
+#   num_array = [1,2,3]
+#   puts "#{num.each} do |2| print x , ** 2  " 
+  
+# end
+# square_array([2,3,4])
