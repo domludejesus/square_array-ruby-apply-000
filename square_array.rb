4707e7b7c8bea9af11a71434de5ee0aa6091f7cc
Dominic DeJesus <domludejesus@gmail.com>
2020-04-08 16:25:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,11 @@
 def square_array(nums)
-  new_array = []
-  nums.each{|x| new_array.push(x**2)}
+  sqr_array = []
+  nums.each{|x| sqr_array.push(x**2)}
   return new_array
 end
 
 nums = [8, 6, 4, 2, 1]
-square_array(nums)
+sqr_array(nums)
 
  
  
